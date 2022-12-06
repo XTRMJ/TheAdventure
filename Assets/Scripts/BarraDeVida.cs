d3536c87e261d8b5f16e9be98458391fdf67e983
Noe9056 <112986319+Noe9056@users.noreply.github.com>
2022-12-06 15:37:41 -0600

    Configuracion de ataque

--- Assets/Scripts/BarraDeVida.cs
+++ Assets/Scripts/BarraDeVida.cs
@@ -10,7 +10,10 @@ public class BarraDeVida : MonoBehaviour
 
     public GameObject[] hearts;
     private int vidas=5;
+    private int vida = 5;
     public GameObject Personaje;
+    public float timer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,34 +23,44 @@ public class BarraDeVida : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ( vidas < 1)
-        {
-            hearts[0].gameObject.SetActive(false);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
-        }
 
-        if (vidas < 2)
-        {
-            hearts[1].gameObject.SetActive(false);
-        }
+        if(vidas < vida){
+            timer = 1.0f;
+            timer = Time.deltaTime;
+            
+            if(timer == 0){
+                if ( vidas < 1)
+                {
+                    hearts[0].gameObject.SetActive(false);
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+                }
 
-        if (vidas < 3)
-        {
-            hearts[2].gameObject.SetActive(false);
-        }
-        if (vidas < 4)
-        {
-            hearts[3].gameObject.SetActive(false);
-        }
-        if (vidas < 5)
-        {
-            hearts[4].gameObject.SetActive(false);
+                if (vidas < 2)
+                {
+                    hearts[1].gameObject.SetActive(false);
+                }
+
+                if (vidas < 3)
+                {
+                    hearts[2].gameObject.SetActive(false);
+                }
+                if (vidas < 4)
+                {
+                    hearts[3].gameObject.SetActive(false);
+                }
+                if (vidas < 5)
+                {
+                    hearts[4].gameObject.SetActive(false);
+                }
+
+            }
+            vida = vida - 1; 
         }
     }
 
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Enemigo")
-            vidas -= 1;
+        vidas -= 1;
     }
 }
